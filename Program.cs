b401dd8e6529b2f34048e3e6284d5bcd89134fef
john <johnathankristhian@gmail.com>
2022-01-15 15:59:11 -0200

    teste dotnet no visual

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace DigitalInnovationOne
+{
+    class program 
+    {
+
+        static void Main(string[] args)
+         {
+                int numeroDeVezes = 10;
+
+                for (int i= numeroDeVezes - 1; i >= 0; i--)
+                {
+               
+                  Console.WriteLine($"Bem vindo ao curso{i}");
+      
+                }
+         
+         }     
+    
+    }
+
+}
